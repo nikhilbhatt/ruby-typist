062902115ce8f906657df5d4e837a4eb2ceeb790
nikhilbhatt <nikhilbhatt931@gmail.com>
2022-08-28 17:04:23 +0530

    [FEATURE] ruby typist gem version 1

---
+++ lib/ruby_typist.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby_typist/version"
+
+module RubyTypist
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ruby_typist_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyTypist do
+  it "has a version number" do
+    expect(RubyTypist::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
